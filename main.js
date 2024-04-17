06b0ea742dc24c8b4ceaafbe4235fa00548bfc8c
soyeniyi801 <naturalbeauty801@gmail.com>
2024-04-17 00:35:22 -0400

    styling and add price

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const burger = document.getElementById('burger')
+const btn = document.querySelector('.button')
+const price = document.getElementById('price')
+
+btn.addEventListener('click', () => {
+ const getBurger = async () => {
+    const response = await fetch('https://bobsburgers-api.herokuapp.com/burgerOfTheDay/5')
+    const data = await response.json();
+    burger.innerHTML = data.name
+    price.innerHTML = data.price
+    console.log(data.name, data.price)
+ }
+ getBurger()
+})
+
