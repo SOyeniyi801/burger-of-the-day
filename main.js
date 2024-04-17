f3b7c4ea6306d8315358987205ace81af4d8c373
soyeniyi801 <naturalbeauty801@gmail.com>
2024-04-17 00:54:11 -0400

    error handling

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const price = document.getElementById("price");
 
 btn.addEventListener("click", () => {
   const getBurger = async () => {
-    const response = await fetch(
-      "https://bobsburgers-api.herokuapp.com/burgerOfTheDay/"
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        "https://bobsburgers-api.herokuapp.com/burgerOfTheDay/"
+      );
+      const data = await response.json();
+      const randomBurger = Math.floor(Math.random() * data.length);
+      console.log(data[randomBurger]);
 
-    const randomBurger = Math.floor(Math.random() * data.length);
-    console.log(data[randomBurger]);
-   
-    burger.innerHTML = data[randomBurger].name
-    price.innerHTML = data[randomBurger].price
+      burger.innerHTML = data[randomBurger].name;
+      price.innerHTML = data[randomBurger].price;
+    } catch {
+      burger.innerHTML = "There was an error";
+    }
   };
   getBurger();
 });
